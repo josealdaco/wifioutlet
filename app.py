786e264b37b7f7a1368e4a2d9d5620f6be75ef09
siko408 <gamerbloxhd@gmail.com>
2019-10-16 19:01:37 -0700

    Able to see account info

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ db = client.WIFIOUTLET
 users = db.users
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def log_page():
     """ Renders LoginPage, First PAGE
@@ -27,6 +28,8 @@ def home_page():
                           {'password': request.form.get('password')})
     user_2 = users.find_one({'email': request.form.get('username')},
                           {'password': request.form.get('password')})
+
+    # If we are returing to the webpage
     if user is not None or user_2 is not None:
         if user is None:
             print("By Email", user_2)
@@ -37,14 +40,60 @@ def home_page():
     else:  # Change the URL later, use url_for rather then rendering template
         return render_template('login_page.html', error=True)
 
+@app.route('/profile/update', methods=['POST'])
+def update_profile():
+    user = users.find_one({'_id': ObjectId(request.form.get('user'))})
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    name = request.form.get('name')
+    lastname = request.form.get('lastname')
+    phone_number = request.form.get('phone_number')
+
+    return render_template('update_profile.html', username=username, email=email, password=password, name=name, lastname=lastname, phone_number=phone_number, user=user)
+
+@app.route('/user_home_page', methods=['POST'])  # Will render the home page for the user
+def user_home_page():
+    user_id = request.form.get('user')
+    user = users.find_one({'_id': ObjectId(user_id)})
+    """ Renders home page for user"""
+    return render_template('user_home_page.html', user=user)
+
+
+@app.route('/money_page', methods=['POST'])  # Will render the money page for user
+def momey_page():
+    """ Renders money page for user"""
+    user_id = request.form.get('user')
+    user = users.find_one({'_id': ObjectId(user_id)})
+    return render_template('money_page.html', user=user)
+
+
+@app.route('/outlet_page', methods=['POST'])  # Will render the outlet page for user
+def outlet_page():
+    user_id = request.form.get('user')
+    user = users.find_one({'_id': ObjectId(user_id)})
+    """ Renders outlet page for user"""
+    return render_template('outlet_page.html', user=user)
+
 
 @app.route('/create_user', methods=['GET'])
 def create_user():
+    """ Directs user to create user page"""
     return render_template('create_page.html')
+
+
+@app.route('/user_profile', methods=['POST'])
+def user_profile():
+    """ Gets user data and renders the page"""
+    user_id = request.form.get('user')
+    print("This is what we get:", user_id)
+    user = users.find_one({'_id': ObjectId(user_id)})
+    return render_template('user_profile.html', user=user, section_len=len(user['Sections']))
 
 
 @app.route('/creation_verify', methods=['POST'])
 def account_verify():
+    """ Verifies if the account being created already exists"""
     username = request.form.get('username')
     password = request.form.get('password')
     email = request.form.get('email')
