26201da0f3a4bef5b0e2148ed26eb4b590e54216
siko408 <gamerbloxhd@gmail.com>
2019-10-18 14:32:19 -0700

    Changing to heroku database

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 """ Below we are setting up our MongoDB Client"""
-client = MongoClient()
-db = client.WIFIOUTLET
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/WIFIOUTLET')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 users = db.users
 app = Flask(__name__)
 
@@ -322,4 +324,4 @@ def account_verify():
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
